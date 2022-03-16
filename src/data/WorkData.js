b3eb0193948d1f3158dc55a01778e1df9b404c4d
Lillith <mactabilis_basium@live.ca>
2022-03-16 14:17:34 -0400

    added work

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,61 +1,21 @@
 export const Work = [
-    {
-        id:1,
-        name:"",
-        description:"",
-        tags:["","",""],
-        
-        demo:"",
-        github:""
-    },
-    {
-        id:2,
-        name:"",
-        description:"",
-        tags:["",""],
-        
-        demo:"",
-        github:""
-        
-    },
-    {
-        id:3,
-        name:"",
-        description:"",
-        tags:["","",""],
-        
-        demo:"",
-        github:""
-    },
-    {
-        id:4,
-        name:"",
-        description:"",
-        tags:["","",""],
-        
-        demo:"",
-        github:""
-       
-    },{
-        id:5,
-        name:"",
-        description:"",
-        tags:["","",""],
-        
-        demo:"",
-        github:""
-        
-    },
-    {
-        id:6,
-        name:"",
-        description:"",
-        tags:["","",""],
-        
-        demo:"",
-        github:""
-        
-    },
-    
-  
-] 
+  {
+    id: 1,
+    name: "Lilly - Imagine a bot",
+    description:
+      "discord bot focused on moderation.Written in javascript with Discord.js librairy.",
+    tags: ["discord", "moderation", "bot"],
+
+    demo: "https://github.com/Lilly3252/lilly",
+    github: "https://github.com/Lilly3252/lilly",
+  },
+  {
+    id: 2,
+    name: "",
+    description: "",
+    tags: ["", ""],
+
+    demo: "",
+    github: "",
+  },
+]; 
